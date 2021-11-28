1c2cd4777ab3fa7e76e91d40612d8a2bf30fd1e2
Zaynokov <vlerii99@mail.ru>
2021-11-28 20:12:00 +0300

    Complete lesson 2-5

--- geekshop/ordersapp/forms.py
+++ geekshop/ordersapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from mainapp.models import Product
 from ordersapp.models import Order
 
 
@@ -28,3 +29,5 @@ class OrderItemForm(forms.ModelForm):
         super(OrderItemForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+        self.fields['product'].queryset = Product.objects.filter(is_active=True, quantity__gte=1)
